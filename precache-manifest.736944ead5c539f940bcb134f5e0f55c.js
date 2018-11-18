dbf636a56a3e0ab5757e091f20bc005c0dd3d910
Jordi Álvaro <jordi.alvaro@ocado.com>
2018-11-18 21:38:19 +0100

    Updates

---
+++ precache-manifest.736944ead5c539f940bcb134f5e0f55c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01322ed9184245041708",
+    "url": "/static/js/main.01322ed9.chunk.js"
+  },
+  {
+    "revision": "f9bfd18a806fd3b27380",
+    "url": "/static/js/1.f9bfd18a.chunk.js"
+  },
+  {
+    "revision": "01322ed9184245041708",
+    "url": "/static/css/main.d3b6acf2.chunk.css"
+  },
+  {
+    "revision": "c9806d5b95f60d510a5754f5f4ba9a52",
+    "url": "/index.html"
+  }
+];
