4314b5ebd462925f158810e288adf5518b4d47a9
Jordi Álvaro <jordi.alvaro@ocado.com>
2018-11-24 13:28:36 +0100

    Increasing the size of the default rectangle

--- src/js/MobileAntennas.js
+++ src/js/MobileAntennas.js
@@ -7,12 +7,12 @@ class MobileAntennas extends React.Component {
     state = {
         selectedArea: {
             pointA: {
-                lat: 41.838103675076,
-                lng: -3.3525553686015
+                lat: 41.738103675076,
+                lng: -3.6525553686015
             },
             pointB: {
-                lat: 41.844144009778,
-                lng: -3.3428779584757
+                lat: 42.044144009778,
+                lng: -3.2428779584757
             }
         },
         antennas: []
